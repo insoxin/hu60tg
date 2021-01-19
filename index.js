59321d9c580edeffb10272904a48241e65895bf8
Redy <192666378@qq.com>
2021-01-20 02:47:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ async function saveRawJson (data) {
   await fs.writeFile(fullPath, JSON.stringify(allHots))
 }
 
-async function sendTgMessage(data) {
+/* async function sendTgMessage(data) {
   const ranks = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣']
   const text = data.splice(1,20).map((o, i) => {
     if (o.promotion) {
@@ -52,8 +52,8 @@ async function sendTgMessage(data) {
     disable_web_page_preview: true
   })
 }
-
-/* async function fetchTrendingDetail (title) {
+ */
+async function fetchTrendingDetail (title) {
   const { data } = await axios.get(`${TRENDING_DETAIL_URL}${title}`)
   const $ = cheerio.load(data)
   return {
@@ -81,4 +81,3 @@ async function sendTgMessage(data) {
 
 bootstrap() 
  
- */
