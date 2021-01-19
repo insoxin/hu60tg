5c7aa29ef47dc853f3ce67c2918dd169bde273da
Redy <192666378@qq.com>
2021-01-20 03:21:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ async function fetchTrendingDetail (title) {
  async function bootstrap () {
   const { data } = await axios.get(TRENDING_URL)
   if (data.currPage === 1) {
-    const items = data.newTopicList[0]
+    const items = data.data.inso[0].card_group
     if (items) {
       for (let item of items) {
         //const { category, desc } = await fetchTrendingDetail(encodeURIComponent(item.desc))
