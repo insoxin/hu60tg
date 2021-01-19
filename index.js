ff878d8387ca6374474c4fec9cc8581a53c45c6c
Redy <192666378@qq.com>
2021-01-20 02:44:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const axios = require('axios')
 const TOKEN = process.env.TOKEN
 const CHANNEL_ID = process.env.CHANNEL_ID
 const TRENDING_URL = 'https://raw.githubusercontent.com/insoxin/hu60tg/main/api/index.index.json'
-const TRENDING_DETAIL_URL = 'https://hu60.cn/q.php/bbs.search.html?keywords='
+const TRENDING_DETAIL_URL = 'https://raw.githubusercontent.com/insoxin/hu60tg/main/api/2021-01-19.json'
 
 const bot = new Telegraf(TOKEN)
 
@@ -53,14 +53,14 @@ async function sendTgMessage(data) {
   })
 }
 
-async function fetchTrendingDetail (title) {
+/* async function fetchTrendingDetail (title) {
   const { data } = await axios.get(`${TRENDING_DETAIL_URL}${title}`)
   const $ = cheerio.load(data)
   return {
     category: $('#pl_topicband dl>dd').first().text(),
     desc: $('#pl_topicband dl>dd').last().text()
   }
-}
+} */
 
  async function bootstrap () {
   const { data } = await axios.get(TRENDING_URL)
