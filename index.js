38d7dc6153ea9fc82faa80d13c75b47b1e1d48bc
Redy <192666378@qq.com>
2021-01-19 23:55:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ async function bootstrap () {
     const items = data.data.cards[0]?.card_group
     if (items) {
       for (let item of items) {
-        const { category, desc } = await fetchTrendingDetail(encodeURIComponent(item.desc))
+        // const { category, desc } = await fetchTrendingDetail(encodeURIComponent(item.desc))
         item.category = category
         item.description = desc
       }
