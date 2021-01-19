d591d7a1510f3c51225bc4411e7f3a554ec1e8aa
Redy <192666378@qq.com>
2021-01-20 03:02:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ async function fetchTrendingDetail (title) {
     desc: $('#pl_topicband dl>dd').last().text()
   }
 }
-
+*/
  async function bootstrap () {
   const { data } = await axios.get(TRENDING_URL)
   if (data.ok === 1) {
@@ -73,7 +73,7 @@ async function fetchTrendingDetail (title) {
         item.description = desc
       }
       await saveRawJson(items)
-      await sendTgMessage(items)
+      //await sendTgMessage(items)
     }
   }
   process.exit(0)
@@ -81,22 +81,4 @@ async function fetchTrendingDetail (title) {
 
 bootstrap() 
  
-*/
- async function bootstrap () {
-  /* const { data } = await axios.get(TRENDING_URL)
-  if (data.ok === 1) {
-    const items = data.data.cards[0].card_group
-    if (items) {
-      for (let item of items) {
-        const { category, desc } = await fetchTrendingDetail(encodeURIComponent(item.desc))
-        item.category = category
-        item.description = desc
-      } */
-      await saveRawJson(items)
-      //await sendTgMessage(items)
-    }
-   /* } */
-  process.exit(0)
-// }
 
-bootstrap() 
