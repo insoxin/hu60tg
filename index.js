cc8abd8feee08c7b74ea0d4bb1a4268d4822dac8
Redy <192666378@qq.com>
2021-01-20 13:29:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ async function saveRawJson (data) {
 
  async function sendTgMessage(data) {
   const ranks = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣']
+  //const ctime = ${dayjs().format('YYYY-MM-DD HH:MM:ss')}
   const text = data.splice(1,20).map((o, i) => {
     if (o.essence === 1) {
       return `🔥<a href="https://hu60.cn/q.php/bbs.topic.${o.id}.html">${o.title}</a> ${(o.read_count / 10000).toFixed(2)} 万`
@@ -48,7 +49,7 @@ async function saveRawJson (data) {
     if (ranks[i]) {
       return `${ranks[i]}<a href="https://hu60.cn/q.php/bbs.topic.${o.id}.html">${o.title}</a> ${(o.read_count / 10000).toFixed(2)} 万`
     }
-    return ` <a href="https://hu60.cn/q.php/bbs.topic.${o.id}.html">${o.title}</a> ${(o.read_count / 10000).toFixed(2)} 万${dayjs('(${o.ctime)}').format('YYYY-MM-DD')}`
+    return ` <a href="https://hu60.cn/q.php/bbs.topic.${o.id}.html">${o.title}</a> ${(o.read_count / 10000).toFixed(2)} 万${o.ctime} `
   })
   text.unshift(`在虎绿林${dayjs().format('YYYY-MM-DD HH:MM:ss')} 首页的帖子`)
   await bot.telegram.sendMessage(CHANNEL_ID, text.join('\n'), {
