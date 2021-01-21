91684cfc53ce43d1e444758aaf4b185608486f18
Redy <192666378@qq.com>
2021-01-21 20:27:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async function saveRawJson (data) {
                                      
 
   ) */
-  const textstat = chat.map((o, i) => {
+  const textstat = chats.map((o, i) => {
    /* if (o.newMsg === 1) {
       return `正在审核`	  
     }
